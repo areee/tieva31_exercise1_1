6d4015414b3fe1a717d947c986024c2f2c163b5b
areee <areee@users.noreply.github.com>
2017-03-09 23:02:08 +0200

    exercise updated (not ready)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,55 @@ import TextField from 'material-ui/TextField'
 import Checkbox from 'material-ui/Checkbox'
 import RaisedButton from 'material-ui/RaisedButton'
 
+// default constants for app:
+const FIRST_NAME = "Pekka";
+const MIDDLE_NAME = "Matti";
+const LAST_NAME = "Virtanen";
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      first: "",
+      middle: "",
+      last: "",
+      useMiddle: false,
+    }
+  }
+
+  updateFirst = (event, newValue) =>{
+    this.setState({
+      first: newValue,
+    });
+  }
+
+  updateMiddle = (event, newValue) =>{
+    this.setState({
+      middle: newValue,
+    });
+  }
+
+  updateLast = (event, newValue) =>{
+    this.setState({
+      last: newValue,
+    });
+  }
+
+  fill = (event) => {
+    this.setState({
+      first: FIRST_NAME,
+      middle: MIDDLE_NAME,
+      last: LAST_NAME,
+    });
+    
+  }
+
+  useMiddleName = (event, isChecked) => {
+    this.setState({
+      useMiddle: isChecked,
+    });
+  }
 
 
   render() {
@@ -18,19 +66,19 @@ class App extends Component {
       <MuiThemeProvider>
         <Paper style={{width: 400, padding: 20}}>
           <div>
-          First name: <TextField defaultValue={""} id="first name"/>
+          First name: <TextField value={this.state.first} onChange={this.updateFirst} id="first name"/>
           </div>
           <div>
-          Middle name: <TextField defaultValue={""} id="middle name"/>
+          Middle name: <TextField value={this.state.middle} onChange={this.updateMiddle} id="middle name"/>
           </div>
           <div>
-          <Checkbox label="Middle name"/>
+          <Checkbox label="Middle name" onCheck={this.useMiddleName}/>
           </div>
           <div>
-          Last name: <TextField defaultValue={""} id="last name"/>
+          Last name: <TextField value={this.state.last} onChange={this.updateLast} id="last name"/>
           </div>
           <div>
-          <RaisedButton label="Autofill" style={{marginLeft: 250}}/>
+          <RaisedButton label="Autofill" style={{marginLeft: 250}} onClick={this.fill}/>
           </div>
           </Paper>
       </MuiThemeProvider>
